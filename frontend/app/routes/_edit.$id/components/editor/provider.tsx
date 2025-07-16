3e7cf69be982871c2fb9401ef0bd0abb8b0ee5bb
Nathan <146884027+npham-dev@users.noreply.github.com>
2025-07-15 18:09:27 -0700

    feat: holy shit it works

--- frontend/app/routes/_edit.$id/components/editor/provider.tsx
+++ frontend/app/routes/_edit.$id/components/editor/provider.tsx
@@ -2,7 +2,9 @@ import { Editor } from "@tiptap/core";
 import Collaboration from "@tiptap/extension-collaboration";
 import CollaborationCaret from "@tiptap/extension-collaboration-caret";
 import { TextStyleKit } from "@tiptap/extension-text-style";
+import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+
 import { Surface, View } from "natmfat";
 import {
   createContext,
@@ -14,10 +16,26 @@ import {
 } from "react";
 import { WebrtcProvider } from "y-webrtc";
 import * as Y from "yjs";
-import { stringToColor } from "~/lib/string-to-color";
+import { omit } from "~/lib/utils";
 import { usePadId } from "../../hooks/use-pad-id";
 import { useUser } from "../../hooks/use-user";
 import { RichTextLink } from "./rich-text-link";
+
+// provider.on("synced", (isSynced) => {
+//   console.log("Synced with peers:", isSynced);
+// });
+// provider.awareness.on("update", () => {
+//   console.log(
+//     "Awareness changed:",
+//     Array.from(provider.awareness.getStates()),
+//   );
+// });
+
+// provider.on("synced", () => {
+//   if (currentEditor.isEmpty) {
+//     currentEditor.commands.setContent("hi");
+//   }
+// });
 
 const EditorContext = createContext<{ editor: Editor | null }>({
   editor: null,
@@ -31,45 +49,22 @@ export const useCurrentEditor = () => {
 export const EditorProvider = (args: { children: ReactNode }) => {
   const user = useUser();
   const padId = usePadId();
-  const [editor, setEditor] = useState<Editor | null>(null);
+  // const [editor, setEditor] = useState<Editor | null>(null);
   const editorRef = useRef<HTMLDivElement>(null);
   const [status, setStatus] = useState("connecting");
 
-  useEffect(() => {
-    if (!editorRef.current) {
-      return;
-    }
+  const [ydoc, setYdoc] = useState<Y.Doc | null>(null);
+  const [provider, setProvider] = useState<WebrtcProvider | null>(null);
 
-    const ydoc = new Y.Doc();
-    const provider = new WebrtcProvider(padId, ydoc, {
-      signaling: [
-        `${import.meta.env.VITE_SIGNALING_SERVER}?token=${user.token}`,
-      ],
-    });
-
-    // provider.on("synced", (isSynced) => {
-    //   console.log("Synced with peers:", isSynced);
-    // });
-    // provider.awareness.on("update", () => {
-    //   console.log(
-    //     "Awareness changed:",
-    //     Array.from(provider.awareness.getStates()),
-    //   );
-    // });
-
-    const nextEditor = new Editor({
+  const editor = useEditor(
+    {
       enableContentCheck: true,
       onContentError: ({ disableCollaboration }) => {
         setStatus("disconnected");
         disableCollaboration();
       },
       onCreate: ({ editor: currentEditor }) => {
-        provider.on("synced", () => {
-          console.log(currentEditor.getText());
-          if (currentEditor.isEmpty) {
-            currentEditor.commands.setContent("hi");
-          }
-        });
+        console.log(currentEditor.getText());
       },
       autofocus: true,
       extensions: [
@@ -80,35 +75,39 @@ export const EditorProvider = (args: { children: ReactNode }) => {
           undoRedo: false,
           link: false,
         }),
-        Collaboration.extend().configure({
-          document: ydoc,
-        }),
-        CollaborationCaret.extend().configure({
-          provider,
-          user: {
-            name: user.name,
-            color: stringToColor(user.name),
-          },
-        }),
+        ydoc && Collaboration.configure({ document: ydoc }),
+        provider &&
+          CollaborationCaret.configure({
+            provider,
+            user: omit(user, ["token"]),
+          }),
+      ].filter((ext) => !!ext),
+    },
+    [ydoc, provider],
+  );
+
+  useEffect(() => {
+    const nextYdoc = new Y.Doc();
+    const nextProvider = new WebrtcProvider(padId, nextYdoc, {
+      signaling: [
+        `${import.meta.env.VITE_SIGNALING_SERVER}?token=${user.token}`,
       ],
     });
 
-    nextEditor.mount(editorRef.current);
-
-    provider.on("status", (e) => {
+    nextProvider.on("status", (e) => {
       setStatus(e.connected ? "connected" : "disconnected");
     });
 
-    setEditor(nextEditor);
+    setYdoc(nextYdoc);
+    setProvider(nextProvider);
 
     return () => {
-      if (nextEditor) {
-        nextEditor.unmount();
-        nextEditor.destroy();
-        setEditor(null);
-      }
-      provider.destroy();
-      ydoc.destroy();
+      nextProvider.disconnect();
+      setProvider(null);
+
+      nextYdoc.destroy();
+      setYdoc(null);
+
       setStatus("disconnected");
     };
   }, [padId, user.token]);
@@ -128,7 +127,7 @@ export const EditorProvider = (args: { children: ReactNode }) => {
             elevated
           >
             {status}
-            <View ref={editorRef}></View>
+            <EditorContent editor={editor}></EditorContent>
             <View className="h-16"></View>
           </Surface>
         </View>
